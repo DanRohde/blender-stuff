339f57e007b7202f747e742479a2329c9a18030a
DanRohde <d.rohde@cms.hu-berlin.de>
2025-10-19 20:55:38 +0200

    removed unused code improved code quality

--- Sierpinski Triangle AddOn/__init__.py
+++ Sierpinski Triangle AddOn/__init__.py
@@ -2,12 +2,10 @@
 # To make this add-on installable, create an extension with it:
 # https://docs.blender.org/manual/en/latest/advanced/extensions/getting_started.html
 
-import bpy
+from bpy import types, ops, data, utils, context, props
 import bmesh
 from bpy.types import Operator
-from bpy.props import FloatVectorProperty
-from bpy.props import IntProperty
-from bpy.props import BoolProperty
+
 
 import math
 from mathutils import Vector
@@ -44,7 +42,7 @@ def create_sierpinski_triangle(level, size):
 
     triangles = subdivide_triangle(vertices, level)
 
-    mesh = bpy.data.meshes.new("SierpinskiTriangle")
+    mesh = data.meshes.new("SierpinskiTriangle")
     bm = bmesh.new()
 
     for triangle in triangles:
@@ -54,8 +52,8 @@ def create_sierpinski_triangle(level, size):
     bm.to_mesh(mesh)
     mesh.update()
 
-    obj = bpy.data.objects.new("SierpinskiTriangle", mesh)
-    bpy.context.collection.objects.link(obj)
+    obj = data.objects.new("SierpinskiTriangle", mesh)
+    context.collection.objects.link(obj)
 
 
 class OBJECT_OT_add_sierpinski_triangle(Operator):
@@ -63,17 +61,17 @@ class OBJECT_OT_add_sierpinski_triangle(Operator):
     bl_label = "Generate Sierpinski Triangle"
     bl_options = {'REGISTER', 'UNDO'}
 
-    level: bpy.props.IntProperty(name="Level", default=3, min=0, max=10)
-    size: bpy.props.FloatProperty(name="Size", default=2.0, min=0.1, max=10.0)
+    level: props.IntProperty(name="Level", default=3, min=0, max=10)
+    size: props.FloatProperty(name="Size", default=2.0, min=0.1, max=10.0)
 
-    def execute(self, context):
+    def execute(self, _context):
         create_sierpinski_triangle(self.level, self.size)
         return {'FINISHED'}
 
 
 def move_vert_to_location(vert,l):
     v=list(vert)
-    return (v[0]+l[0],v[1]+l[1],v[2]+l[2])
+    return v[0]+l[0],v[1]+l[1],v[2]+l[2]
 
 def get_obj_size(obj):
     bbox_corners = [obj.matrix_world @ Vector(corner) for corner in obj.bound_box]
@@ -87,18 +85,18 @@ def get_obj_size(obj):
     size_x = max_x - min_x
     size_y = max_y - min_y
     size_z = max_z - min_z
-    return (size_x, size_y, size_z)
+    return size_x, size_y, size_z
     
 def clone_obj(obj, orig_data, location):
     mesh_copy = orig_data.copy()
     obj_copy = obj.copy()
     obj_copy.data = mesh_copy
     obj_copy.location = location
-    bpy.context.collection.objects.link(obj_copy)
-    bpy.context.view_layer.objects.active = obj
+    context.collection.objects.link(obj_copy)
+    context.view_layer.objects.active = obj
     obj.select_set(True)
     obj_copy.select_set(True)
-    bpy.ops.object.join()
+    ops.object.join()
     return obj
             
 def create_pyramid(mesh, location, size):
@@ -127,21 +125,21 @@ def mult_dist_factor(f,t):
     return tuple(newlist)
 
 def create_sierpinski_pyramid(self):
-    orig_obj = bpy.context.active_object
+    orig_obj = context.active_object
     if self.useselection and orig_obj and orig_obj.type == 'MESH':
         mesh = orig_obj.data.copy()
         orig_data = orig_obj.data  
     else:
-        mesh = bpy.data.meshes.new("Sierpinski Pyramid")
+        mesh = data.meshes.new("Sierpinski Pyramid")
         create_pyramid(mesh, Vector((0, 0, 0)), self.size)
         orig_data=mesh.copy()
         
     # deselect all
-    for obj in bpy.context.selected_objects:
+    for obj in context.selected_objects:
         obj.select_set(False)
     
-    obj = bpy.data.objects.new("Sierpinski Pyramid", mesh)
-    bpy.context.collection.objects.link(obj)
+    obj = data.objects.new("Sierpinski Pyramid", mesh)
+    context.collection.objects.link(obj)
 
     obj_size = get_obj_size(obj)
     for i in range(self.iterations):
@@ -157,78 +155,67 @@ def create_sierpinski_pyramid(self):
         orig_data = obj.data.copy()
     return obj
 
-class OBJECT_OT_add_sierpinski_pyramid(bpy.types.Operator):
+class OBJECT_OT_add_sierpinski_pyramid(types.Operator):
     """Add a Sierpinski Pyramid"""
     bl_idname = "mesh.add_sierpinski_pyramid"
     bl_label = "Add Sierpinski Pyramid"
     bl_options = {'REGISTER', 'UNDO'}
 
    
-    iterations: bpy.props.IntProperty(
+    iterations: props.IntProperty(
         name="Iterations",
         description="Number of iterations for the Sierpinski Pyramid",
         default=2,
         min=0,
         max=5,
     )
-    size: bpy.props.FloatProperty(
+    size: props.FloatProperty(
         name="Pyramid Size",
         description="Size of the Sierpinski Pyramid",
         default=2.0,
         min=0.1,
         max=10.0,
     )
-    distfactor: bpy.props.FloatProperty(
+    distfactor: props.FloatProperty(
         name="Distance factor",
         description="Adjust the distance between base objects",
         default=1.0,
     )
-    useselection: bpy.props.BoolProperty(
+    useselection: props.BoolProperty(
         name="Use selection",
         description="Use selection to create a Sierpinski pyramid",
         default=True,
     ) 
-    def execute(self, context):
+    def execute(self, _context):
         create_sierpinski_pyramid(self)
         return {'FINISHED'}
 
 
 # Registration
 
-def add_triangle_button(self, context):
+def add_triangle_button(self, _context):
     self.layout.operator(
         OBJECT_OT_add_sierpinski_triangle.bl_idname,
         text="Sierpinski Triangle",
         icon='PLUGIN',
     )
-def add_pyramid_button(self, context):
+def add_pyramid_button(self, _context):
     self.layout.operator(
         OBJECT_OT_add_sierpinski_pyramid.bl_idname,
         text="Sierpinski Pyramid",
         icon='PLUGIN',
     )
-
-
-# This allows you to right click on a button and link to documentation
-#def add_object_manual_map():
-#    url_manual_prefix = "https://docs.blender.org/manual/en/latest/"
-#    url_manual_mapping = (
-#        ("bpy.ops.mesh.add_object", "scene_layout/object/types.html"),
-#    )
-#    return url_manual_prefix, url_manual_mapping
-
-
 def register():
-    bpy.utils.register_class(OBJECT_OT_add_sierpinski_triangle)
-    bpy.utils.register_class(OBJECT_OT_add_sierpinski_pyramid)
-    bpy.types.VIEW3D_MT_mesh_add.append(add_triangle_button)
-    bpy.types.VIEW3D_MT_mesh_add.append(add_pyramid_button)
+    utils.register_class(OBJECT_OT_add_sierpinski_triangle)
+    utils.register_class(OBJECT_OT_add_sierpinski_pyramid)
+    types.VIEW3D_MT_mesh_add.append(add_triangle_button)
+    types.VIEW3D_MT_mesh_add.append(add_pyramid_button)
 
 def unregister():
-    bpy.utils.unregister_class(OBJECT_OT_add_sierpinski_triangle)
-    bpy.utils.unregister_class(OBJECT_OT_add_sierpinski_pyramid)
-    bpy.types.VIEW3D_MT_mesh_add.remove(add_triangle_button)
-    bpy.types.VIEW3D_MT_mesh_add.remove(add_pyramid_button)
+    utils.unregister_class(OBJECT_OT_add_sierpinski_triangle)
+    utils.unregister_class(OBJECT_OT_add_sierpinski_pyramid)
+    types.VIEW3D_MT_mesh_add.remove(add_triangle_button)
+    types.VIEW3D_MT_mesh_add.remove(add_pyramid_button)
 
 
 if __name__ == "__main__":
