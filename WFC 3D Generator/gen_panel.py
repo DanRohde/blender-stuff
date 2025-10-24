16032625bb9231b3dc770ade1bb8ee1ba0ecd043
DanRohde <d.rohde@cms.hu-berlin.de>
2025-10-24 12:06:48 +0200

    added missing render time output

--- WFC 3D Generator/gen_panel.py
+++ WFC 3D Generator/gen_panel.py
@@ -43,6 +43,10 @@ class WFC3DGeneratePanel(bpy.types.Panel):
         col = row.column()
         col.operator("object.wfc_3d_generate_stop_delayed_renderer", icon='EVENT_MEDIASTOP')
         col.enabled = props.running_delayed_renderer
+        if props.render_delay>0:
+            row = box.row()
+            render_time = props.render_delay * props.grid_size[0] * props.grid_size[1] * props.grid_size[2]
+            row.label(text=f"Est. render time: {render_time:.2f} second(s).")
 
         box = layout.box()
         box.prop(props, "random_start_cell")
