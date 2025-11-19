2ae44a33c6dca8badc43da8969e7014c80399745
DanRohde <d.rohde@cms.hu-berlin.de>
2025-11-19 20:46:25 +0100

    completed first version of WFC Backup

--- WFC 3D Generator/backup_operators.py
+++ WFC 3D Generator/backup_operators.py
@@ -3,10 +3,47 @@ import bpy
 import json
 from bpy.props import StringProperty
 from bpy_extras.io_utils import ImportHelper, ExportHelper
-from .helper import get_default_empty_name
+from .helper import get_default_empty_name, get_default_empty_object
+
+def remove_existing_constraints(obj):
+    for p in obj.keys():
+        if p.startswith("wfc_"):
+            del obj[p]
+
+def put_data_into_object(data, p, obj):
+    try:
+        iter(data)
+        if '_object_link_' in data:
+            obj[p] = bpy.data.objects[data["_object_link_"]]
+        else:
+            if len(data) == 1:
+                obj[p] = data[0]
+            else:
+                obj[p] = data
+    except Exception:
+        obj[p] = data
 
 def import_data(props, data):
-    pass
+    collection = props.collection_obj
+    default_obj = get_default_empty_object(collection, True)
+
+    #if props.backup_import_replace: remove_existing_constraints(default_obj)
+    for p in data["defaults"]:
+        if p in default_obj and not props.backup_import_overwrite: continue
+        put_data_into_object(data["defaults"][p], p, default_obj)
+
+    for o in data["objects"]:
+        if o in collection.objects:
+            obj = collection.objects[o]
+        elif o in collection.children:
+            obj = get_default_empty_object(collection.children[o], True)
+        else:
+            continue
+        #if props.backup_import_replace: remove_existing_constraints(obj)
+        for p in data["objects"][o]:
+            if p in obj and not props.backup_import_overwrite: continue
+            put_data_into_object(data["objects"][o][p], p, obj)
+
 class WFC3D_OT_ImportJson(bpy.types.Operator, ImportHelper):
     bl_idname = "wfc3d.import_json"
     bl_label = "Import Constraints"
@@ -21,17 +58,17 @@ class WFC3D_OT_ImportJson(bpy.types.Operator, ImportHelper):
                 data = json.load(fp)
 
             import_data(context.scene.wfc_props, data)
-            self.report({'INFO'}, f"Imported constraints from json file {fp}.")
+            self.report({'INFO'}, f"Imported constraints from json file {fp.name}.")
             return {'FINISHED'}
         except Exception as e:
-            self.report({'ERROR'}, "Import constraints from json file failed")
+            self.report({'ERROR'}, f"Import constraints from json file failed: {e}")
             return {'CANCELLED'}
 
 def get_export_data(props):
     data = {
         'version' : '1.0',
         'creator' : 'WFC 3D Generator extension for Blender',
-        'default' : {},
+        'defaults' : {},
         'objects' : {},
     }
     for obj in props.collection_obj.objects:
@@ -40,19 +77,19 @@ def get_export_data(props):
             if p.startswith("wfc_"):
                 try:
                     iter(obj[p])
-                    prop_data[p] = list(obj[p])
-                except Exception as e:
+                    if len(obj[p]) == 1:
+                        prop_data[p] = obj[p][0]
+                    else:
+                        prop_data[p] = list(obj[p])
+                except Exception:
                     if isinstance(obj[p], bpy.types.Object):
                         prop_data[p] = { '_object_link_' : obj[p].name }
                     else:
                         prop_data[p] = obj[p]
         if obj.name.startswith(get_default_empty_name()):
-            data['default'] = prop_data
+            data['defaults'] = prop_data
         else:
             data["objects"][obj.name] = prop_data
-
-
-    print(f"data={data}")
     return data
 class WFC3D_OT_ExportJson(bpy.types.Operator, ExportHelper):
     bl_idname = "wfc3d.export_json"
