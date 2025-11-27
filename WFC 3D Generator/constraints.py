6377050ab7839f34dcc1e3b284821d9a4a90363e
DanRohde <d.rohde@cms.hu-berlin.de>
2025-11-27 07:17:17 +0100

    fixed distance from object constraints

--- WFC 3D Generator/constraints.py
+++ WFC 3D Generator/constraints.py
@@ -583,6 +583,7 @@ class WFC3DConstraints:
                                 if df == 0 and self.constraints[o]["distance_object"][i] is None: continue
                                 if df == 2 and self.constraints[o]["distance_subcollection"][i] is None: continue
                                 if abs(position[0]-x) > distance[0] and abs(position[1]-y) > distance[1] and abs(position[2]-z) > distance[2]: continue
+                                if ( df == 0 and self.constraints[o]["distance_object"][i] != obj_name) or (df == 2 and self.constraints[o]["distance_subcollection"][i] != obj_name): continue
                                 self.grid.remove_obj(o, (x,y,z))
 
     def set_cache_geometry_compare(self, current_obj, obj, direction, result):
