1a30b5ba3e40d2b36b5b369404d83b7ec5d748e8
DanRohde <d.rohde@cms.hu-berlin.de>
2025-12-05 08:35:54 +0100

    added constraints reset to backup tool panel

--- WFC 3D Generator/backup_panel.py
+++ WFC 3D Generator/backup_panel.py
@@ -27,4 +27,13 @@ class WFC3D_PT_BackupPanel(bpy.types.Panel):
 
         box.operator("wfc3d.import_json")
 
+        layout.separator()
+        box = layout.box()
+        box.row().label(text="Reset All WFC3D Generator Constraints")
+        row = box.row()
+        row.prop(props, "reset_all_confirmation")
+        col = row.column()
+        col.operator("object.wfc_reset_all_constraints").sure = props.reset_all_confirmation
+        col.enabled = props.reset_all_confirmation
+
 panels = [ WFC3D_PT_BackupPanel ]
