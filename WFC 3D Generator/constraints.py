ad1b6f112665c7e7101d20e7fd4af4e8704a3aea
DanRohde <d.rohde@cms.hu-berlin.de>
2025-12-13 15:26:46 +0100

    next try to fix frequency constraints bugs

--- WFC 3D Generator/constraints.py
+++ WFC 3D Generator/constraints.py
@@ -547,13 +547,15 @@ class WFC3DConstraints:
         x, y, z = cell
         if len(self.grid.grid[x,y,z])==0: return []
         current_obj = self.grid.grid[x,y,z][0]
+        dimensions = self.constraints[current_obj]["dim_xyz"]
+        dim_fac = dimensions[0] * dimensions[1] * dimensions[2]
         # grid frequency
         if self.constraints[current_obj]["freq_grid"]>-1:
-            count = self.grid.count_obj(current_obj)
+            count = self.grid.count_obj(current_obj) / dim_fac
             if count >= self.constraints[current_obj]["freq_grid"]: self.grid.remove_obj(current_obj)
 
         if self.constraints[current_obj]["freq_grid_pct"] > -1:
-            count = self.grid.count_obj(current_obj)
+            count = self.grid.count_obj(current_obj) / dim_fac
             max_count = self.constraints[current_obj]["freq_grid_pct"]/100 * self.grid.grid_size[0] * self.grid.grid_size[1] * self.grid.grid_size[2]
             if count >= max_count: self.grid.remove_obj(current_obj)
 
